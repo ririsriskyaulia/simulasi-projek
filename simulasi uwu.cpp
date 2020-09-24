c1817896f1d13c4646c3fe0a2a6ad883dd19347b
riris <ririsriskyaulia@gmail.com>
2020-09-24 13:55:00 -0800

    ini ya

--- simulasi uwu.cpp
+++ simulasi uwu.cpp
@@ -5,14 +5,12 @@ using namespace std;
 int main(){
 	
 	cout<<"HAI SEMWUA";
-<<<<<<< HEAD
+
 	cout << "werwererwerwerrr";
-=======
+
 	cout<<"hi";
 	cout<<"hi juga";
-<<<<<<< HEAD
->>>>>>> 7b2364d56237e09c57b4e2b2eb78a96979dd0c6e
-=======
+
 	cout<<"asdasdasdasdasdasdasdasdasdasdasdasd";
->>>>>>> 15ed6a9451c58e548fa673fa476a83b8bbe333fe
+
 }
